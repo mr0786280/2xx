793f040626fc0a48881a1df52445148b7346b232
Monica <monicajromero5683@hotmail.com>
2018-09-16 20:29:51 -0400

    Went back to change any missed html comments to php comments.

--- index.php
+++ index.php
@@ -1,39 +1,34 @@
-<?php
-  /*
-  	The following will be passed along to JavaScript by establishing variables in PHP and passing them into the HTML via Global JavaScript variables the same names used for PHP.
-  */
+		<?php
+			/* The following will be passed along to JavaScript by establishing variables in PHP and passing them into the HTML via Global JavaScript variables the same names used for PHP.*/
 
-	$siteName = 'inside out | 2xx';
-
-
-?> 
-
-
-
+			$siteName = 'inside out | 2xx';
+		?> 
 
 <!doctype html>
 
-<!-- this is beginning of the html the browser will process -->
+		<?php
+			/* this is beginning of the html the browser will process*/
+		?>
 <html>
-
-<!-- information used by the browser -->
-
+		<?php
+			/*information used by the browser*/
+		?>
 <head>
 
 
     <meta charset="utf-8">
-
-    <!--
-            displayed anywhere the title is used
-            for the browser, search engines, etc.
-        -->
+	
+		<?php
+    		/*displayed anywhere the title is used for the browser, search engines, etc.*/
+		?>
+	
     <title><?php echo $siteName = 'inside out | 2xx'; ?></title>
-
-    <!--
-            used to display information about the 
-            page by search engine and  other sites./
-            looking at our page.
-        -->
+	
+		<?php
+			/*used to display information about the 
+            page by search engine and other sites by looking at our page*/
+  		?>
+	
     <meta name="description" content="inside out is a class project we use to learn html." />
     <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 	
@@ -42,9 +37,11 @@
 	</script>
 	
 </head>
-
-<!-- content displayed in the browser window -->
-
+	
+	<?php
+		/* content displayed in the browser window*/
+	?>
+	
 <body>
 
     <header>
