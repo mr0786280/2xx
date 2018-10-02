0e86f025c235fa80996fc0d8bb8ab20e406e829d
Monica <monicajromero5683@hotmail.com>
2018-10-02 17:26:41 -0400

    Began the top dynamic navigation.

--- README.md
+++ README.md
@@ -13,3 +13,13 @@ Version 201.0
 	- Removed header and footer from the pages replaced.
 	- Created the include folder
 	Created the header.inc.php and foot.inc.php files.
+	
+	
+Version 202.0
+	- replaced navigation manu with top level pages by creating a function and array to dynamically create the menu (love, who, challenges, you)
+	- created the function.inc.php file
+		-created the menuBuilder function
+	- created the menu.data.php file
+		- created menuItems array
+	- included in the very top of the header.inc.php a require_once to the include/menu.data.php file
+	- include in the very top of the header.inc.php a require_once to the include/functions.inc.php file
--- include/header.inc.php
+++ include/header.inc.php
@@ -1,8 +1,16 @@
-		<?php
-			/* The following will be passed along to JavaScript by establishing variables in PHP and passing them into the HTML via Global JavaScript variables the same names used for PHP.*/
+<?php
+
+	/* Pulls data elements used thoughout the entire website */
+	require_once 'menu.data.php';
+
+	/* Pulls functions used throughout the entire website. */
+	require_once 'functions.inc.php';
+
+	
+	/* The following will be passed along to JavaScript by establishing variables in PHP and passing them into the HTML via Global JavaScript variables the same names used for PHP. */
 
 			$siteName = 'inside out | 2xx';
-		?> 
+?> 
 
 <!doctype html>
 
@@ -47,51 +55,10 @@
     <header>
         <h1><a href="index.php"><?php echo $siteName = 'inside out | 2xx'; ?></a></h1>
         <h2>continuously falling foward into the light... </h2>
-        <nav>
-            <ul>
-                <li><a href="love.php">love</a>
-                    <ul>
-                        <li><a href="things.html">things</a>
-                            <ul>
-                                <li><a href="music.html">music</a></li>
-                                <li><a href="movies.html">movies</a></li>
-                                <li><a href="books.html">books</a></li>
-                                <li><a href="website.html">website</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="people.html">people</a>
-                            <ul>
-                                <li><a href="family.html">family</a></li>
-                                <li><a href="friends.html">friends</a></li>
-                                <li><a href="others.html">others</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="who.php">who</a>
-                    <ul>
-                        <li><a href="i.html">i</a>
-                            <ul>
-                                <li><a href="was then.html">was then</a></li>
-                                <li><a href="am now.html">am now</a></li>
-                                <li><a href="will be.html">will be</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="challenges.php">challenges</a>
-                    <ul>
-                        <li><a href="behind me.html">behind me</a></li>
-                        <li><a href="ahead of me.html">ahead of me</a></li>
-                    </ul>
-                </li>
-                <li><a href="you.php">you</a>
-                    <ul>
-                        <li><a href="profile.html">profile</a></li>
-                        <li><a href="settings.html">settings</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+		
+		<nav>
+			<?php echo menuBuilder($menuItems); ?>
+		</nav>
+
 
     	</header>
